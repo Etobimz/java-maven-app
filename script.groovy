67bc96c1bb5a7e82504f8424dfca34c52b1259c9
Abimbola_O <39372915+Etobimz@users.noreply.github.com>
2025-07-30 13:12:04 -0600

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -12,13 +12,12 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId:'docker-gub-maven-rep', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD' )])
         {
-            sh '''
-            echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-            docker build -t java-maven-app:2.0 .
-            docker tag java-maven-app:2.0 $DOCKER_USERNAME/java-maven-app:2.0
-            docker push $DOCKER_USERNAME/java-maven-app:2.0
-
-            '''
+           sh """
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t java-maven-app:${env.IMAGE_NAME} .
+docker tag java-maven-app:${env.IMAGE_NAME} \$DOCKER_USERNAME/java-maven-app:${env.IMAGE_NAME}
+docker push \$DOCKER_USERNAME/java-maven-app:${env.IMAGE_NAME}
+"""
          }
 
 
