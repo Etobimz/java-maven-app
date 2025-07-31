16ee5ece9afba83b72db48c62aff6704e4f1d911
Abimbola_O <39372915+Etobimz@users.noreply.github.com>
2025-07-31 14:09:14 -0600

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,36 +1,22 @@
 def buildJar() {
-
     echo "Building the application..."
-                    sh 'mvn package'
-
-
-
+    sh 'mvn package'
 }
 
 def buildImage() {
-
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId:'docker-gub-maven-rep', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD' )])
-        {
-           sh """
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t java-maven-app:${env.IMAGE_NAME} .
-docker tag java-maven-app:${env.IMAGE_NAME} \$DOCKER_USERNAME/java-maven-app:${env.IMAGE_NAME}
-docker push \$DOCKER_USERNAME/java-maven-app:${env.IMAGE_NAME}
-"""
-         }
-
-
+    echo "Building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-gub-maven-rep', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+        sh """
+            echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USERNAME" --password-stdin
+            docker build -t java-maven-app:${env.IMAGE_NAME} .
+            docker tag java-maven-app:${env.IMAGE_NAME} \$DOCKER_USERNAME/java-maven-app:${env.IMAGE_NAME}
+            docker push \$DOCKER_USERNAME/java-maven-app:${env.IMAGE_NAME}
+        """
+    }
 }
-
-
 
 def deployApp() {
-
-     echo "Deploying the application..."
+    echo "Deploying the application..."
 }
-
-
-
 
 return this
